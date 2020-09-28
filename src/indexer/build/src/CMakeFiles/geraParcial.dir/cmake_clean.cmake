d25c5419c6a35070a5867b449893ceb85ba8a3f7
Roberto Cidade Fonseca <rcidadef@gmail.com>
2020-09-28 10:55:32 -0400

    [chore] move files

---
+++ src/indexer/build/src/CMakeFiles/geraParcial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geraParcial.dir/run/geraParcial.o"
+  "geraParcial"
+  "geraParcial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/geraParcial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/indexer/build/src/CMakeFiles/indexer.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rcidadef/repos/Waves/Waves/indexer/src/DescritorColecaoWBR.cc" "/home/rcidadef/repos/Waves/Waves/indexer/build/src/CMakeFiles/indexer.dir/DescritorColecaoWBR.o"
+  "/home/rcidadef/repos/Waves/Waves/indexer/src/DescritorInfoweb.cc" "/home/rcidadef/repos/Waves/Waves/indexer/build/src/CMakeFiles/indexer.dir/DescritorInfoweb.o"
+  "/home/rcidadef/repos/Waves/Waves/indexer/src/HashDoc.cc" "/home/rcidadef/repos/Waves/Waves/indexer/build/src/CMakeFiles/indexer.dir/HashDoc.o"
+  "/home/rcidadef/repos/Waves/Waves/indexer/src/Heap.cc" "/home/rcidadef/repos/Waves/Waves/indexer/build/src/CMakeFiles/indexer.dir/Heap.o"
+  "/home/rcidadef/repos/Waves/Waves/indexer/src/IndiceInvertido.cc" "/home/rcidadef/repos/Waves/Waves/indexer/build/src/CMakeFiles/indexer.dir/IndiceInvertido.o"
+  "/home/rcidadef/repos/Waves/Waves/indexer/src/SegmentaURL.cc" "/home/rcidadef/repos/Waves/Waves/indexer/build/src/CMakeFiles/indexer.dir/SegmentaURL.o"
+  "/home/rcidadef/repos/Waves/Waves/indexer/src/Vocabulario.cpp" "/home/rcidadef/repos/Waves/Waves/indexer/build/src/CMakeFiles/indexer.dir/Vocabulario.o"
+  "/home/rcidadef/repos/Waves/Waves/indexer/src/prefixinmem.cc" "/home/rcidadef/repos/Waves/Waves/indexer/build/src/CMakeFiles/indexer.dir/prefixinmem.o"
+  "/home/rcidadef/repos/Waves/Waves/indexer/src/utils/jenkins_hash.cc" "/home/rcidadef/repos/Waves/Waves/indexer/build/src/CMakeFiles/indexer.dir/utils/jenkins_hash.o"
+  "/home/rcidadef/repos/Waves/Waves/indexer/src/valstringuniao_utf8.cc" "/home/rcidadef/repos/Waves/Waves/indexer/build/src/CMakeFiles/indexer.dir/valstringuniao_utf8.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "indexer_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "/include"
+  "/include/linux"
+  "../../libs-ext/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
